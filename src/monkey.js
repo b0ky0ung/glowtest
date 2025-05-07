e27b706507d41e97adc1958748c1a95cd6d7cc7b
배보경 <bokyung@baebogyeong-ui-MacBookPro.local>
2025-05-07 14:01:04 +0900

    feat: 퀴즈 문항 추가

--- src/monkey.js
+++ src/monkey.js
@@ -1,15 +1,25 @@
 const monkeyData = {
-    title: "나와 닮은 동물 찾기",
-    description: "당신과 닮은 동물을 찾아보세요!",
+    title: "동물 문해력 테스트",
+    description: "당신의 문해력과 닮은 동물을 찾아보세요!",
     type: "point",
     startButtonText: "테스트 시작",
     data: [
       {
-        "question": "친구가 약속에 늦었다. 당신의 반응은?",
+        "question": "1. “그는 결국 이를 악물고 해냈다.”의 뜻은??",
         "options": [
-          { "text": "괜찮아~", "score": 1 },
-          { "text": "조금 짜증남", "score": 2 },
-          { "text": "화남", "score": 3 }
+          { "text": "이를 닦으며 끝냈다", "score": 0 },
+          { "text": "억지로 참고 해냈다", "score": 1 },
+          { "text": "입을 다물고 도망쳤다", "score": 0 },
+          { "text": "이를 악용해서 성공했다", "score": 0 }
+        ]
+      },
+      {
+        "question": "2. 다음 중 단어 의미가 잘못 연결된 것은?",
+        "options": [
+          { "text": "분쟁 - 시끄럽고 복잡한 다툼", "score": 0 },
+          { "text": "적용 - 알맞게 이용하거나 맞춤남", "score": 0 },
+          { "text": "대등 - 비슷하거나 견줄 수 있음", "score": 0 },
+          { "text": "조정 - 상대를 마음대로 다루는 일", "score": 1 }
         ]
       }
       // 추가 질문...
@@ -17,8 +27,8 @@ const monkeyData = {
     result: [
       {
         "range": [0, 5],
-        "title": "곰 같은 성격",
-        "description": "느긋하고 온화해요 🐻"
+        "title": "강아지? 🐶",
+        "description": "느당신 혹시 우리집 뽀삐...? 🐾"
       },
       {
         "range": [6, 10],
