76aa06f97c933b46d8b807b30be626ddc9ae40ed
배보경 <bokyung@baebogyeong-ui-MacBookPro.local>
2025-05-07 12:04:29 +0900

    fix: 퀴즈 버튼에 option_btn 클래스 추가하여 스타일 적용

--- js/common.js
+++ js/common.js
@@ -35,7 +35,9 @@ class TestPage {
       const options = this.question[this.current].options;
       for (let i = 0; i < options.length; i++) {
         const button = document.createElement('button');
+        button.className = 'option_btn';  // ✅ class 추가!
         button.innerHTML = options[i].text;
+
   
         button.addEventListener('click', () => {
           if (this.type === "point") {
